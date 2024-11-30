9150e1389a6f24745fb4e34ae21176c46c3602ca
Fabio Carvalho <fabio_carvalho242@outlook.com>
2024-11-30 16:04:31 -0300

    Criação do pojeto API SpringBoot Bater Ponto

---
+++ src/main/java/com/assemblyconsultoria/baterponto/BaterpontoApplication.java
@@ -0,0 +1,13 @@
+package com.assemblyconsultoria.baterponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaterpontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaterpontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assemblyconsultoria/baterponto/BaterpontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assemblyconsultoria.baterponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaterpontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
